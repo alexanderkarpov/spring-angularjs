a0c508c5e3d0b77888d08af4b3b79203311d045b
Alexander Karpov <alexander.karpoff@gmail.com>
2017-10-18 07:42:45 +0300

    boilerplate

---
+++ src/main/java/com/example/springangularjs/SpringAngularJSApplication.java
@@ -0,0 +1,12 @@
+package com.example.springangularjs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAngularJSApplication {
+
+	public static void main(String... args) {
+		SpringApplication.run(SpringAngularJSApplication.class, args);
+	}
+}
