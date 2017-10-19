0e7a65dad02827c21a28880d93794d55723e46de
Alexander Karpov <alexander.karpoff@gmail.com>
2017-10-19 22:31:29 +0300

    boilerplate

---
+++ src/main/resources/static/js/hello.js
@@ -0,0 +1,8 @@
+(function () {
+    'use strict';
+
+    angular.module('hello', [])
+        .controller('home', function ($scope) {
+            $scope.greeting = {id: 'xxx', content: 'Hello World!'}
+        });
+})();
